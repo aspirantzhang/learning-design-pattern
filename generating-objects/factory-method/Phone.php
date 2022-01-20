527f8e07c57bee648b8cf8e7faf7bd284eeb6b9d
aspirantzhang <admin@aspirantzhang.com>
2022-01-20 14:54:33 +0800

    chore: typo fixed

--- generating-objects/factory-method/Phone.php
+++ generating-objects/factory-method/Phone.php
@@ -7,5 +7,5 @@ namespace zhang;
 abstract class Phone
 {
     abstract public function getPlatform(): Platform; // Attention here, the return type is Platform
-    // here is refactor method
+    // here is factory method
 }
